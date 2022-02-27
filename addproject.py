bc87110681b6a2600938aac2fb04f209274d56df
royroyroy555in <royroyroy555in@rediff.com>
2022-02-28 02:35:12 +0530

    second commit

--- addproject.py
+++ addproject.py
@@ -14,6 +14,22 @@ def royadd(arg1,arg2):
 
 royadd(34,89)
 
+def roypower(arg1,arg2):
+    power = math.pow(arg1,arg2)
+    print('Power is ', power)
+
+roypower(2,2)
+
+p = int(input('PLease Enter The First Number: '))
+q = int(input('Please Enter The Second Number: '))
+
+r = p + q
+
+print('The addition of the two added numbers is', r)
+
+Access Key ID = "AHTSBN563md5Ht"
+Secret Key ID = "B23n55HST193B"
+
 
 
 
